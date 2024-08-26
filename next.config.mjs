79c5b6525a67889a98f426a95ca36be1d2f6301d
Noel Bank <bank@neoskop.de>
2024-08-26 17:50:54 +0200

    feat: add basic packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    reactCompiler: true,
+    ppr: 'incremental',
+    after: true,
+  },
+};
 
 export default nextConfig;
